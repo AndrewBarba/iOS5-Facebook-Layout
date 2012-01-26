675a84f12c938a09ce2a172b323c31e39e4a2cf3
Andrew Barba <andrewbarba@ABarba-MBP.(none)>
2012-01-26 12:39:52 -0500

    Initial Commit

---
+++ Facebook Nav/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Facebook Nav
+//
+//  Created by Andrew Barba on 1/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Facebook Nav/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Facebook Nav
+//
+//  Created by Andrew Barba on 1/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
